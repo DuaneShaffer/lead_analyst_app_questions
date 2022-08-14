befc0646598b95776a242e946682246898459ad9
dlshaffer <dlshaffer@alaska.edu>
2022-08-14 15:51:28 -0800

    Add docstring

--- inclusive_integer.py
+++ inclusive_integer.py
@@ -1,4 +1,5 @@
 def is_inclusive_integer(range, input_integer):
+    """ Given a lower and upper bound as range, return if input integer is between them inclusive."""
     lower_bound, upper_bound = range
     if upper_bound < lower_bound:
         raise ValueError("Upper bound must be >= lower bound")
