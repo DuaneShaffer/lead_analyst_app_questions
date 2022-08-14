d50160b9bd76978e7347ee43f5989b93d2347a6d
dlshaffer <dlshaffer@alaska.edu>
2022-08-14 15:24:20 -0800

    lower edge case test

--- inclusive_integer_tests.py
+++ inclusive_integer_tests.py
@@ -3,9 +3,11 @@ from inclusive_integer import is_inclusive_integer
 
 class InclusiveIntTests(unittest.TestCase):
 
-    def test_upper(self):
+    def test_basic(self):
         self.assertEqual(is_inclusive_integer((10, 20), 15), True)
 
+    def test_edge_case_lower(self):
+        self.assertEqual(is_inclusive_integer((10, 20), 10), True)
 
 if __name__ == '__main__':
     unittest.main()
