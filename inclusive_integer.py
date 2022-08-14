e523bfa8867e55b0ef10699c291436575d341975
dlshaffer <dlshaffer@alaska.edu>
2022-08-14 15:42:02 -0800

    Throw error for invalid range

--- inclusive_integer.py
+++ inclusive_integer.py
@@ -1,3 +1,5 @@
 def is_inclusive_integer(range, input_integer):
     lower_bound, upper_bound = range
+    if upper_bound < lower_bound:
+        raise ValueError("Upper bound must be >= lower bound")
     return lower_bound <= input_integer <= upper_bound
