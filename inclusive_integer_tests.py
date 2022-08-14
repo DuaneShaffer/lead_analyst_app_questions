ea71e360073da8c07e117b299fd00d722e2daf80
dlshaffer <dlshaffer@alaska.edu>
2022-08-14 15:25:50 -0800

    refactor test names

--- inclusive_integer_tests.py
+++ inclusive_integer_tests.py
@@ -3,13 +3,13 @@ from inclusive_integer import is_inclusive_integer
 
 class InclusiveIntTests(unittest.TestCase):
 
-    def test_basic(self):
+    def basic_true(self):
         self.assertEqual(is_inclusive_integer((10, 20), 15), True)
 
-    def test_edge_case_lower(self):
+    def edge_case_lower(self):
         self.assertEqual(is_inclusive_integer((10, 20), 10), True)
 
-    def test_edge_case_upper(self):
+    def edge_case_upper(self):
         self.assertEqual(is_inclusive_integer((10, 20), 20), True)
 
 if __name__ == '__main__':
