f2d8f09a582994c7f3e0ad4e5b3c34f1034b2bf4
dlshaffer <dlshaffer@alaska.edu>
2022-08-14 15:48:18 -0800

    Refactor basic tests together

--- inclusive_integer_tests.py
+++ inclusive_integer_tests.py
@@ -3,17 +3,15 @@ from inclusive_integer import is_inclusive_integer
 
 class InclusiveIntTests(unittest.TestCase):
 
-    def test_basic_true(self):
+    def test_basic(self):
         self.assertEqual(is_inclusive_integer((10, 20), 15), True)
+        self.assertEqual(is_inclusive_integer((10, 20), 0), False)
 
     def test_edge_case_lower(self):
         self.assertEqual(is_inclusive_integer((10, 20), 10), True)
 
     def test_edge_case_upper(self):
         self.assertEqual(is_inclusive_integer((10, 20), 20), True)
-
-    def test_basic_false(self):
-        self.assertEqual(is_inclusive_integer((10, 20), 0), False)
 
     def test_impossible_range(self):
         with self.assertRaises(ValueError):
