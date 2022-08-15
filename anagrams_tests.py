75211388452fa290a527ff9ccd580b654ebc2670
dlshaffer <dlshaffer@alaska.edu>
2022-08-14 19:15:28 -0800

    Move one of those tests since it's not basic

--- anagrams_tests.py
+++ anagrams_tests.py
@@ -4,8 +4,11 @@ from anagrams import are_anagrams
 class InclusiveIntTests(unittest.TestCase):
 
     def test_basic(self):
-        self.assertEqual(are_anagrams('dormitory', 'dirty room'), True)
+
         self.assertEqual(are_anagrams('agile', 'development'), False)
+
+    def test_with_spaces(self):
+        self.assertEqual(are_anagrams('dormitory', 'dirty room'), True)
 
 if __name__ == '__main__':
     unittest.main()
